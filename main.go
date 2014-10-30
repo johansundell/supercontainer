8d981c94740c99dbe29fc3a4d78acfd8ab0eff05
Johan Sundell <johan@pixpro.net>
2014-10-30 12:29:58 +0100

    Added support for worker pool

--- main.go
+++ main.go
@@ -2,21 +2,107 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
+	"sync"
 )
+
+// TODO: rename type to a better name
+type data struct {
+	from     string
+	to       string
+	filename string
+	//err      error
+}
+
+type result struct {
+	data
+	synced bool
+	err    error
+}
 
 func main() {
 	var from, to string
+	var useMaxCPU bool
+	var workers int
 	flag.StringVar(&from, "from", "/home/johan/tmp/hackad", "The directory to sync from")
 	flag.StringVar(&to, "to", "/home/johan/tmp/supercontainer", "The directory to sync to")
+	flag.BoolVar(&useMaxCPU, "useMaxCPU", false, "Use max CPU")
+	flag.IntVar(&workers, "workerPoolSize", 400, "The number of the worker pool size")
 	flag.Parse()
 
-	listDir(from, to)
+	if yes, _ := exists(from); !yes {
+		fmt.Println("Error, from does not exists")
+		return
+	}
+
+	if useMaxCPU {
+		nCPU := runtime.NumCPU()
+		runtime.GOMAXPROCS(nCPU)
+		fmt.Println("Number of CPUs:", nCPU)
+	}
+
+	done := make(chan struct{})
+	defer close(done)
+
+	datac, errc := listDir(done, from, to)
+
+	c := make(chan result)
+	var wg sync.WaitGroup
+	const numDigesters = 400
+	wg.Add(numDigesters)
+	for i := 0; i < numDigesters; i++ {
+		go func() {
+			digester(done, datac, c)
+			wg.Done()
+		}()
+	}
+	go func() {
+		wg.Wait()
+		close(c)
+	}()
+
+	filesSynced := 0
+	for r := range c {
+		if r.err != nil {
+			fmt.Println("Error:", r.err)
+		}
+		if r.synced {
+			filesSynced++
+		}
+	}
+
+	fmt.Println("Files synced", filesSynced)
+
+	if err := <-errc; err != nil {
+		fmt.Println("Failed", err)
+	}
+}
+
+func digester(done <-chan struct{}, datac <-chan data, c chan<- result) {
+	for d := range datac {
+		var err error = nil
+		synced := false
+		// TODO: If the file exists, do a md5 checksum to see if the file is diffrent
+		if yes, _ := exists(d.to + string(os.PathSeparator) + d.filename); !yes {
+			err = createDirAndCopy(d.from, d.to, d.filename)
+			if err == nil {
+				synced = true
+			}
+		}
+
+		select {
+		case c <- result{d, synced, err}:
+		case <-done:
+			return
+		}
+	}
 }
 
 func createDirAndCopy(from, to, filename string) error {
@@ -30,34 +116,46 @@ func createDirAndCopy(from, to, filename string) error {
 	return cp(to+string(os.PathSeparator)+filename, from+string(os.PathSeparator)+filename)
 }
 
-func listDir(fromPath, toPath string) {
+func listDir(done <-chan struct{}, fromPath, toPath string) (<-chan data, <-chan error) {
 	start := len(strings.Split(fromPath, string(os.PathSeparator)))
-	if yes, _ := exists(fromPath); !yes {
-		fmt.Println("Error, from does not exists")
-		return
-	}
-	filepath.Walk(fromPath, func(path string, info os.FileInfo, err error) error {
-		if !info.IsDir() && info.Mode().IsRegular() {
-			arr := strings.Split(path, string(os.PathSeparator))
 
-			sep := string(os.PathSeparator)
-			if len(arr[start:len(arr)-1]) == 0 {
-				sep = ""
+	datac := make(chan data)
+	errc := make(chan error, 1)
+
+	go func() {
+
+		defer close(datac)
+
+		errc <- filepath.Walk(fromPath, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
 			}
 
-			from := strings.Join(arr[:len(arr)-1], string(os.PathSeparator))
-			to := toPath + sep + strings.Join(arr[start:len(arr)-1], string(os.PathSeparator)) +
-				string(os.PathSeparator) + info.Name()
+			if !info.IsDir() && info.Mode().IsRegular() {
+				arr := strings.Split(path, string(os.PathSeparator))
 
-			// TODO: If the file exists, do a md5 checksum to see if the file is diffrent
-			if yes, _ := exists(to + string(os.PathSeparator) + info.Name()); !yes {
-				if err := createDirAndCopy(from, to, info.Name()); err != nil {
-					fmt.Println("Copy error", err)
+				sep := string(os.PathSeparator)
+				if len(arr[start:len(arr)-1]) == 0 {
+					sep = ""
+				}
+
+				d := data{}
+
+				d.from = strings.Join(arr[:len(arr)-1], string(os.PathSeparator))
+				d.to = toPath + sep + strings.Join(arr[start:len(arr)-1], string(os.PathSeparator)) +
+					string(os.PathSeparator) + info.Name()
+				d.filename = info.Name()
+
+				select {
+				case datac <- d:
+				case <-done:
+					return errors.New("walk canceled")
 				}
 			}
-		}
-		return nil
-	})
+			return nil
+		})
+	}()
+	return datac, errc
 }
 
 func exists(path string) (bool, error) {
